f51a5d739ce40a76c3449a24d7f9ace700433593
luke-rocha3 <lucasrx2410@gmail.com>
2026-05-21 09:23:31 -0300

    Prática 2 – Parte 1: Finalizada. Telas criadas e estrutura organizada.

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,47 +1,20 @@
-package com.example.weatherapp
+    package com.example.weatherapp
 
-import android.app.Activity
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import com.example.weatherapp.ui.theme.WeatherAppTheme
+    import android.os.Bundle
+    import androidx.activity.ComponentActivity
+    import androidx.activity.compose.setContent
+    import androidx.compose.material3.ExperimentalMaterial3Api
+    import com.example.weatherapp.ui.screens.HomePage
+    import com.example.weatherapp.ui.theme.WeatherAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            WeatherAppTheme {
-                // Captura a activity atual para o botão Sair
-                val activity = LocalContext.current as Activity
-                HomePage(activity)
+    @OptIn(ExperimentalMaterial3Api::class)
+    class MainActivity : ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContent {
+                WeatherAppTheme {
+                    HomePage()
+                }
             }
         }
     }
-}
-
-@Composable
-fun HomePage(activity: Activity) {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Bem-vindo(a) ao WeatherApp!", style = MaterialTheme.typography.headlineMedium)
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(onClick = {
-            // O botão sai da MainActivity e volta para o Login
-            activity.finish()
-        }) {
-            Text("Sair")
-        }
-    }
-}
