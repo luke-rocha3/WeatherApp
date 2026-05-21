66067a2e7f7e1e79313d2b4ea5fc80cde0360782
luke-rocha3 <lucasrx2410@gmail.com>
2026-05-21 16:02:43 -0300

    Pratica 4 Parte 4: Localização do usuário

--- app/src/main/java/com/example/weatherapp/ui/screens/MapPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/MapPage.kt
@@ -1,13 +1,20 @@
 package com.example.weatherapp.ui.screens
 
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.example.weatherapp.MainViewModel
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -23,9 +30,21 @@ fun MapPage(
 
     val camPosState = rememberCameraPositionState()
 
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
     GoogleMap(
         modifier = modifier.fillMaxSize(),
         cameraPositionState = camPosState,
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true),
         onMapClick = {
             viewModel?.add("Cidade@${it.latitude}:${it.longitude}", location = it)
         }
