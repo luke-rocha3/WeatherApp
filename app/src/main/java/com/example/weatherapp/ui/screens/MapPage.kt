61274de0bd577ed17e6d9fc3d5ab55d80654f312
luke-rocha3 <lucasrx2410@gmail.com>
2026-07-09 17:29:34 -0300

    Remove marcadores hardcoded do MapPage (resolve #1)

--- app/src/main/java/com/example/weatherapp/ui/screens/MapPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/MapPage.kt
@@ -10,8 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import com.example.weatherapp.MainViewModel
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
@@ -24,10 +22,6 @@ fun MapPage(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel? = null
 ) {
-    val recife = remember { MarkerState(LatLng(-8.05, -34.9)) }
-    val caruaru = remember { MarkerState(LatLng(-8.27, -35.98)) }
-    val joaopessoa = remember { MarkerState(LatLng(-7.12, -34.84)) }
-
     val camPosState = rememberCameraPositionState()
 
     val context = LocalContext.current
@@ -58,23 +52,5 @@ fun MapPage(
                 )
             }
         }
-        Marker(
-            state = recife,
-            title = "Recife",
-            snippet = "Marcador em Recife",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
-        )
-        Marker(
-            state = caruaru,
-            title = "Caruaru",
-            snippet = "Marcador em Caruaru",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
-        )
-        Marker(
-            state = joaopessoa,
-            title = "João Pessoa",
-            snippet = "Marcador em João Pessoa",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
-        )
     }
 }
